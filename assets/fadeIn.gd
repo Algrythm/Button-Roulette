6f11aa4394826f4866eda6821d058fcd63a38a94
mars <69923391+Algrythm@users.noreply.github.com>
2026-08-03 02:06:34 -0600

    Delete assets directory

--- assets/fadeIn.gd
+++
@@ -1,9 +0,0 @@
-extends ColorRect
-
-@onready var animationPlayer = $AnimationPlayer
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	animationPlayer.play("fade")
-	await get_tree().create_timer(1.0).timeout
-	$".".queue_free()
